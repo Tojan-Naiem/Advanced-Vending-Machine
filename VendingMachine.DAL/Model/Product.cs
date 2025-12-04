859490cb7e28763f963b2bf79323aa5726ebbe3c
Tojan-Naiem <tojan050@gmail.com>
2025-12-04 23:02:00 +0200

    feat : add baseModel

---
+++ VendingMachine.DAL/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.DAL.Model
+{
+    public class Product:BaseModel
+    {
+    }
+}
