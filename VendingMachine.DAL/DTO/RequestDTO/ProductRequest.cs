20c7c2b278ee28bab7ab50b32c74410e9875d052
Tojan-Naiem <tojan050@gmail.com>
2025-12-04 23:26:00 +0200

    feat : add product request and response

---
+++ VendingMachine.DAL/DTO/RequestDTO/ProductRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace VendingMachine.DAL.DTO.RequestDTO
+{
+    public class ProductRequest
+    {
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public int Quantity { get; set; }
+        public IFormFile? MainImage { get; set; }
+
+    }
+}
---
+++ VendingMachine.DAL/DTO/ResponseDTO/ProductResponse.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VendingMachine.DAL.DTO.ResponseDTO
+{
+    public class ProductResponse
+    {
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public int Quantity { get; set; }
+        [JsonIgnore]
+        public string MainImage { get; set; } = string.Empty;
+        public string MainImageUrl => $"https://localhost:7039/Images/{MainImage}";
+    }
+}
