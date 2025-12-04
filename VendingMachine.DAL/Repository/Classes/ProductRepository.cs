0b43f4f7350d4b7b373bdf32c3ac9a4eb0899a5b
Tojan-Naiem <tojan050@gmail.com>
2025-12-04 23:46:53 +0200

    feat  : add product repository with interface addingn save/get/getAll/delete

---
+++ VendingMachine.DAL/Repository/Classes/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.DAL.Data;
+using VendingMachine.DAL.DTO.RequestDTO;
+using VendingMachine.DAL.Model;
+using VendingMachine.DAL.Repository.Interfaces;
+
+namespace VendingMachine.DAL.Repository.Classes
+{
+    public class ProductRepository:IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProductRepository(
+            ApplicationDbContext dbContext
+            )
+        {
+            _dbContext = dbContext;
+        }
+        public async Task SaveAsync(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+         
+        }
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
+        public async Task<Product?> GetProductAsync(long id)
+        {
+            return await _dbContext.Products.FindAsync(id);
+        }
+        public async Task RemoveAsync(Product product)
+        {
+             _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
