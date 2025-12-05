b954e0eabca2a0452d888b29b321224468883e7d
Tojan-Naiem <tojan050@gmail.com>
2025-12-05 15:37:51 +0200

    feat : add product controller with CRUD operation

--- VendingMachine.BLL/Service/Interfaces/IProductService.cs
+++ VendingMachine.BLL/Service/Interfaces/IProductService.cs
@@ -13,8 +13,8 @@ namespace VendingMachine.BLL.Service.Interfaces
         public Task<long> CreateFile(ProductRequest request);
         public Task<bool> DeleteFile(long id);
         public Task<bool> UpdateProductAsync(long id, ProductRequest request);
-        public Task<List<ProductResponse>> GetAll();
-        public ProductResponse? GetById(long id);
-        public Task<bool> ToggleStatus(long id);
+        public Task<List<ProductResponse>> GetAllAsync();
+        public Task<ProductResponse?> GetByIdAsync(long id);
+        public Task<bool> ToggleStatusAsync(long id);
     }
 }
