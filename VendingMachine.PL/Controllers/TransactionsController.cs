cc1e245c20534ea96034e4d53744c7ab48c0753d
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 01:15:14 +0200

    feat : add transaction controller

---
+++ VendingMachine.PL/Controllers/TransactionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VendingMachine.BLL.Service.Interfaces;
+using VendingMachine.DAL.DTO.RequestDTO;
+
+namespace VendingMachine.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionsController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+        [HttpPost("")]
+        public async Task<IActionResult> Add([FromBody] TransactionRequest request)
+        {
+            var result = await _transactionService.CreateTransactionAsync(request);
+            return result ? Ok("Done") : BadRequest();
+        }
+    }
+}
