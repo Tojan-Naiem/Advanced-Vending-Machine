a880f7feced1c99e6880e5ba2117834f14b7f24a
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 00:33:06 +0200

    feat : add methods into transaction repo

--- VendingMachine.DAL/Repository/Interfaces/ITransactionRepository.cs
+++ VendingMachine.DAL/Repository/Interfaces/ITransactionRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VendingMachine.DAL.Model;
 
 namespace VendingMachine.DAL.Repository.Interfaces
 {
     public interface ITransactionRepository
     {
+        public Task SaveAsync(Transaction request);
+        public Task<List<Transaction>> GetProductsAsync();
+        public Task<Transaction?> GetProductAsync(long id);
+        public Task RemoveAsync(Transaction request);
+        public Task SaveChangesInDatabase();
     }
 }
