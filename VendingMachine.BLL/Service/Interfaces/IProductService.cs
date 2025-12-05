e23b80d9f5194e3cd4e5b400c9e238e3dde51314
Tojan-Naiem <tojan050@gmail.com>
2025-12-05 13:45:43 +0200

    feat :  add methods into IProductService

--- VendingMachine.BLL/Service/Interfaces/IProductService.cs
+++ VendingMachine.BLL/Service/Interfaces/IProductService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VendingMachine.DAL.DTO.RequestDTO;
 
 namespace VendingMachine.BLL.Service.Interfaces
 {
     public interface IProductService
     {
+        public Task<long> CreateFile(ProductRequest request);
+        public Task<bool> DeleteFile(long id);
     }
 }
