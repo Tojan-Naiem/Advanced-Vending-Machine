b7226c66c15695ff9a0ec25a4f50988f46edf9c5
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 00:28:02 +0200

    feat : add transaction request and response

---
+++ VendingMachine.DAL/DTO/ResponseDTO/TransactionResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.DAL.Model;
+
+namespace VendingMachine.DAL.DTO.ResponseDTO
+{
+    public class TransactionResponse
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
