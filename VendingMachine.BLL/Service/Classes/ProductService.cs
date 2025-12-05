b6506bad68e79457ed14dcd5e6a70b2a35b0a617
Tojan-Naiem <tojan050@gmail.com>
2025-12-05 13:42:54 +0200

    feat : add Product Service with Create and Delete product with Image

---
+++ VendingMachine.BLL/Service/Classes/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.BLL.Service.Interfaces;
+using VendingMachine.DAL.DTO.RequestDTO;
+using VendingMachine.DAL.DTO.ResponseDTO;
+using VendingMachine.DAL.Model;
+using VendingMachine.DAL.Repository.Classes;
+using VendingMachine.DAL.Repository.Interfaces;
+
+namespace VendingMachine.BLL.Service.Classes
+{
+    public class ProductService : IProductService
+    {
+        private readonly IFileService _fileService;
+        private readonly IProductRepository _productRepository;
+        public ProductService(
+            IProductRepository productRepository,
+            IFileService fileService
+            ) 
+        {
+            _productRepository = productRepository;
+            _fileService = fileService;
+        }
+        public async Task<long> CreateFile(ProductRequest request)
+        {
+            var entity = request.Adapt<Product>();
+            if (request.MainImage is not null)
+            {
+                var imagePath = await _fileService.UploadAsync(request.MainImage);
+                entity.MainImage = imagePath;
+            }
+            await _productRepository.SaveAsync(entity);
+            return entity.Id;
+        }
+        public async Task<bool> DeleteFile(long id)
+        {
+            var entity = await _productRepository.GetProductAsync(id);
+            bool successDelete = await _fileService.DeleteAsync(entity!.MainImage);
+            if (successDelete is false)
+                throw new Exception("Error");
+            await _productRepository.RemoveAsync(entity);
+            return true;
+        }
+        
+    
+
+}
+}
