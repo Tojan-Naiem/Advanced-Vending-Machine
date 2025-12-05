e20ddda8d3529c91629aaf49db8552ea3b2ee8d8
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 01:42:16 +0200

    feat : add checkout response

---
+++ VendingMachine.BLL/Service/Interfaces/ICheckOutService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.BLL.Service.Interfaces
+{
+    internal interface ICheckOutService
+    {
+    }
+}
