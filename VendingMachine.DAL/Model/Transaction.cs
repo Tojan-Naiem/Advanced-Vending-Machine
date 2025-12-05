c6d05fa2f38c810c8b8871de8510e6c00b922bdb
Tojan-Naiem <tojan050@gmail.com>
2025-12-05 23:47:49 +0200

    feat : add transaction model with payment method enum

---
+++ VendingMachine.DAL/Model/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.DAL.Model
+{
+    public enum PaymentMethod
+    {
+        Success,
+        Pending,
+        Failed
+    }
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public long ProductId { get; set; }
+        public Product Product { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
