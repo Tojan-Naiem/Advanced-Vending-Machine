7dd6942adc4e4a0ccaeb3e8e34cca78e1a55443c
Tojan-Naiem <tojan050@gmail.com>
2025-12-05 13:35:15 +0200

    feat : add fileService

---
+++ VendingMachine.BLL/Service/Interfaces/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.BLL.Service.Interfaces
+{
+    public interface IFileService
+    {
+        public Task<string> UploadAsync(IFormFile file);
+        public Task<bool> DeleteAsync(string fileName);
+    }
+}
