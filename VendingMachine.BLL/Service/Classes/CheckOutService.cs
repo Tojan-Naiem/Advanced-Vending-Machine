0fa73029cb9c0fd03cba0eb95cae233cb45219df
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 02:16:03 +0200

    feat : add email sender utils

--- VendingMachine.BLL/Service/Classes/CheckOutService.cs
+++ VendingMachine.BLL/Service/Classes/CheckOutService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace VendingMachine.BLL.Service.Classes
 {
-    internal class CheckOutService
+    public class CheckOutService
     {
     }
 }
---
+++ VendingMachine.PL/Utils/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace VendingMachine.PL.Utils
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public EmailSender(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("tojan050@gmail.com", "jcyl dfoq utwz ulwp")
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: "tojan050@gmail.com",
+                                to: email,
+                                subject,
+                                htmlMessage
+                                )
+                { IsBodyHtml = true });
+        }
+    }
+}
