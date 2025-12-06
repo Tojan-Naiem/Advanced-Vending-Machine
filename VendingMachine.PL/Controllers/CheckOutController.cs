20de3800e7ffb24a4b37e5686f23dea3a71e6684
Tojan-Naiem <tojan050@gmail.com>
2025-12-06 02:43:22 +0200

    fix : fix checkout controller

--- VendingMachine.PL/Controllers/CheckOutController.cs
+++ VendingMachine.PL/Controllers/CheckOutController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
+using VendingMachine.DAL.DTO.RequestDTO;
+using VendingMachine.BLL.Service.Classes;
+using VendingMachine.BLL.Service.Interfaces;
 namespace VendingMachine.PL.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CheckOutController : ControllerBase
     {
+        private readonly ICheckOutService _checkOutService;
+        public CheckOutController(ICheckOutService checkOutService)
+        {
+            _checkOutService = checkOutService;
+        }
+        [HttpPost("payment")]
+        public async Task<IActionResult> Payment([FromBody] CheckOutRequest request)
+        {
+            var response = await _checkOutService.ProcessPaymentAsync(request, Request);
+            return Ok(response);
+        }
     }
 }
