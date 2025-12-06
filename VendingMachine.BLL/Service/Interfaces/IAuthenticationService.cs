ccda8e07f42b96eded647cd5c14273758e98b429
lama Ali <lama.rafatali@gmail.com>
2025-12-06 23:44:53 +0200

    Implemented AuthenticationService login + JWT token

---
+++ VendingMachine.BLL/Service/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.DAL.DTO.RequestDTO;
+using VendingMachine.DAL.DTO.ResponseDTO;
+
+namespace VendingMachine.BLL.Service.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<string> ConfirmEmail(string token, string userId);
+        Task<UserResponse> LoginAsync(LoginRequest request);
+
+    }
+}
--- VendingMachine.PL/Program.cs
+++ VendingMachine.PL/Program.cs
@@ -1,35 +1,79 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using StackExchange.Redis;
 using VendingMachine.BLL.Service.Classes;
 using VendingMachine.BLL.Service.Interfaces;
 using VendingMachine.DAL.Data;
+using VendingMachine.DAL.Model;
 using VendingMachine.DAL.Repository.Classes;
 using VendingMachine.DAL.Repository.Interfaces;
 using VendingMachine.DAL.Utils;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("Redis"); 
-    options.InstanceName = "VendingMachine_"; 
-});
+
+
 builder.Services.AddScoped<IFileService,FileService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ISeedData, SeedData>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 
-    ));
+//  Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Configuration.AddUserSecrets<Program>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var jwtKey = builder.Configuration["Jwt:SecretKey"];
+var gmailPassword = builder.Configuration["GmailPassword"];
+var stripeKey = builder.Configuration["Stripe:SecretKey"];
+
+//  Identity
+builder.Services.AddIdentity<Users, IdentityRole>(Options =>
+{
+    Options.Password.RequireDigit = true;
+    Options.Password.RequiredLength = 8;
+    Options.Password.RequireLowercase = true;
+    Options.Password.RequireNonAlphanumeric = false;
+    Options.User.RequireUniqueEmail = true;
+    Options.Lockout.MaxFailedAccessAttempts = 5;
+    Options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders().AddSignInManager().AddRoles<IdentityRole>();
+   
+
+//Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+    };
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,12 +81,16 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+//  Apply Seeding
 var scope = app.Services.CreateScope();
 var objectOfSeedData = scope.ServiceProvider.GetRequiredService<ISeedData>();
 await objectOfSeedData.DataSeedingAsync();
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
