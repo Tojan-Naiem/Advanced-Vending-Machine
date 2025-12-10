165450726d9253a823ccc6d21bec4593056009ef
Tojan-Naiem <tojan050@gmail.com>
2025-12-11 00:20:22 +0200

    fix : move enums folder into DAL layer

---
+++ VendingMachine.DAL/Enums/MachineEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.BLL.Service.Enums
+{
+
+    public enum MachineEvent
+    {
+
+        QR_Scanned,
+        Item_Selected,
+        Cancel,
+        Payment_Received,
+        Payment_Failed,
+        Dispense_Complete,
+        Error_Occurred
+
+    }
+}
+
---
+++ VendingMachine.DAL/Enums/MachineState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.BLL.Service.Enums
+{
+    public enum MachineStateType
+    {
+        Idle,
+        WaitingForQR,
+        WaitingForPayment,
+        ProcessingPayment,
+        DispensingItem,
+        ReturningChange,
+        Error
+    }
+}
