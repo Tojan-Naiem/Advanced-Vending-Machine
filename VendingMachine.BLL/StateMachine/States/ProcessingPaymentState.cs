1a1b140b8e959fa4669c2d24440eecbf8fa06c22
Tojan-Naiem <tojan050@gmail.com>
2025-12-11 00:46:37 +0200

    update : update ProcessingPaymentState

--- VendingMachine.BLL/StateMachine/States/ProcessingPaymentState.cs
+++ VendingMachine.BLL/StateMachine/States/ProcessingPaymentState.cs
@@ -12,10 +12,15 @@ namespace VendingMachine.BLL.StateMachine.States
     {
         public MachineStateType StateType => MachineStateType.ProcessingPayment;
 
-        public void HandleEvent(VendingMachineContext context, MachineEvent evt, object? data = null)
+        public void HandleEvent(VendingMachineContext context, MachineEvent evt)
         {
-            if (evt == MachineEvent.Dispense)
+            if (evt == MachineEvent.Payment_Received)
                 context.SetState(new DispensingItemState());
+            else if (evt == MachineEvent.Error_Occurred)
+                context.SetState(new ErrorState());
+            // if there's no event and an error occurred
+            else
+                Console.WriteLine($" Event {evt} ignored in ProcessingPaymentState state");
         }
     }
 }
