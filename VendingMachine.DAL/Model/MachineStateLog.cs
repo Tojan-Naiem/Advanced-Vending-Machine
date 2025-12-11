20a5928baff1d61b39a3b83e27a9ae68ef9a277b
Tojan-Naiem <tojan050@gmail.com>
2025-12-11 17:15:44 +0200

    feat : add machine state log model

---
+++ VendingMachine.DAL/Model/MachineStateLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.DAL.Model
+{
+    public class MachineStateLog
+    {
+        public long Id { get; set; }
+        public string? StateBefore { get; set; }
+
+        public string StateAfter { get; set; } = string.Empty;
+
+        public string? EventTriggered { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
