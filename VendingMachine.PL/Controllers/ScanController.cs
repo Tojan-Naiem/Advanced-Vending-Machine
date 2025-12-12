66a4be1ca3a75eedbc0c085b223b454236fc08ac
Tojan-Naiem <tojan050@gmail.com>
2025-12-12 20:06:18 +0200

    feat : add scan controller

---
+++ VendingMachine.PL/Controllers/ScanController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VendingMachine.BLL.Service.Interfaces;
+using VendingMachine.DAL.DTO.RequestDTO;
+
+namespace VendingMachine.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+        private readonly IVendingMachineService _vmService;
+        public ScanController( IVendingMachineService vmService)
+        {
+            _vmService = vmService;
+
+        }
+        [HttpPost("scan-qr")]
+        public async Task<IActionResult> ScanQr([FromBody] ScanRequest request)
+        {
+            await _vmService.TriggerAsync(DAL.Enums.MachineEvent.QR_Scanned);
+            return Ok();
+        }
+    }
+}
